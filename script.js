8d3cc9beaa7e8df3e3be14ca08080a2a1157f735
JakeCervantes <108781163+JakeCervantes@users.noreply.github.com>
2023-08-07 20:29:36 -0700

    Additional Functionality
    Now their is an overlay for a win/tie. Where the correct winner is announced or a tie is announced and the user must interact with an overlay button to restart the game. Last thing needed to complete the project is give the user options for custom names and a way to reset the game at any point during the game.

--- script.js
+++ script.js
@@ -10,10 +10,20 @@ const cells=[
   document.querySelector('.cell9')
 ];
 
+cells.forEach(cell => {
+  cell.addEventListener('click', gameController);
+});
 
 const player1= playerFactory('player1', 'X');
 const player2=playerFactory('player2', 'O');
-let currentPlayer = player1;
+let currentPlayer;
+
+//handles displaying a clear indication of the end of a game
+let blurOverlay = document.querySelector('.blur-overlay');
+let displayOutcome = document.querySelector('.game-over');
+let resetBtn = document.getElementById('reset-button');
+resetBtn.addEventListener('click', clearPopUp);
+let gameOverMessage = displayOutcome.querySelector('p');
 
 function playerFactory(name, marker) {
   return {
@@ -22,12 +32,22 @@ function playerFactory(name, marker) {
   };
 }
 
-const gameInit = (function () {
-  cells.forEach(cell => {
-    cell.addEventListener('click', gameController);
-  });
+function clearPopUp() {
+  blurOverlay.style.display='none';
+  displayOutcome.style.display='none';
+  gameInit();
+}
 
-}) ();
+function gameInit() {
+  currentPlayer=player1;
+  cells.forEach(cell => {
+    cell.innerHTML='';
+    if(cell.getAttribute('data-marked') !== null)
+    {cell.removeAttribute('data-marked');}
+  })
+}
+
+gameInit();
 
 
 function gameController(event) {
@@ -46,15 +66,41 @@ function gameController(event) {
 
     currentPlayer = currentPlayer === player1 ? player2 : player1;
 
+
+      //handles the event of a win
     if (winOccured)
     {
-      setTimeout(() => {
-        alert(winningPlayer.name + ' wins!');
-      }, 50);
-      
+      popUP(winningPlayer.name);
     }
+
+      //handles the event of a tie
+    const markedCells = cells.filter(cell => cell.getAttribute('data-marked'));
+    if (markedCells.length === cells.length)
+    {
+      popUP('tie');
+    }; // All cells are marked
   }
 }
+
+function popUP(event) {
+  blurOverlay.style.display='block';
+  displayOutcome.style.display='block';
+  gameOverMessage.innerText=event;
+}
+
+
+
+function resetGame() {
+  cells.forEach(cell => {
+    cell.innerHTML='';
+    if(cell.getAttribute('data-marked') !== null)
+    {cell.removeAttribute('data-marked');}
+  })
+
+  currentPlayer=player1;
+}
+
+
 
 function checkWin(marker) {
   // Check rows
